ae9f7a264306fc36e4ae2af378a728b509a6fb34
Varad Deshmukh <varad.deshmukh@colorado.edu>
2018-11-26 07:29:49 -0700

    1. OOPize the data retreival. 2. Make batches more balanced.

--- data.py
+++ data.py
@@ -9,29 +9,41 @@ import gc
 import imp
 import pandas as pd
 
-def read_data():
-  train_data = pd.read_csv('../../data/volcanoes/train_images.csv', header=None);
-  train_labels = pd.read_csv('../../data/volcanoes/train_labels.csv');
-  train_labels_onehot = np.zeros((train_labels.shape[0], 2));
-  train_labels_onehot[:,0] = (train_labels['Volcano?'].values == 0)*1;
-  train_labels_onehot[:,1] = (train_labels['Volcano?'].values == 1)*1;
+class VolcanoData: 
 
-  test_data = pd.read_csv('../../data/volcanoes/test_images.csv', header=None);
-  test_labels = pd.read_csv('../../data/volcanoes/test_labels.csv');
-  test_labels_onehot = np.zeros((test_labels.shape[0], 2));
-  test_labels_onehot[:,0] = (test_labels['Volcano?'].values == 0)*1;
-  test_labels_onehot[:,1] = (test_labels['Volcano?'].values == 1)*1;
-  
-  return [train_data.values, train_labels_onehot, test_data.values, test_labels_onehot];
+  def __init__(self):
+    self.train_data = pd.read_csv('../../data/volcanoes/train_images.csv', header=None);
+    train_labels = pd.read_csv('../../data/volcanoes/train_labels.csv');
+    self.train_labels_onehot = np.zeros((train_labels.shape[0], 2));
+    self.train_labels_onehot[:,0] = (train_labels['Volcano?'].values == 0)*1;
+    self.train_labels_onehot[:,1] = (train_labels['Volcano?'].values == 1)*1;
 
-def get_next_batch(data, labels, batch_size):
-    idx = np.arange(data.shape[0]);
-    np.random.shuffle(idx);
-    idx = np.array(idx[:batch_size]);
+    self.pos_idx = np.where(train_labels['Volcano?'].values == 1)[0];
+    self.neg_idx = np.where(train_labels['Volcano?'].values == 0)[0];
     
-    return data[idx], labels[idx]; 
+    self.ratio = self.pos_idx.shape[0] / (self.pos_idx.shape[0] + self.neg_idx.shape[0]);
+    #self.ratio = 0.5;
 
-[train_data, train_labels, test_data, test_labels] = read_data();
+    self.test_data = pd.read_csv('../../data/volcanoes/test_images.csv', header=None);
+    test_labels = pd.read_csv('../../data/volcanoes/test_labels.csv');
+    self.test_labels_onehot = np.zeros((test_labels.shape[0], 2));
+    self.test_labels_onehot[:,0] = (test_labels['Volcano?'].values == 0)*1;
+    self.test_labels_onehot[:,1] = (test_labels['Volcano?'].values == 1)*1;
+        
+  def get_next_batch(self, batch_size):
+      
+      np.random.shuffle(self.pos_idx);
+      np.random.shuffle(self.neg_idx);
+      num_pos = int(self.ratio*batch_size);  
+      num_neg = batch_size - num_pos; 
 
-data, labels = get_next_batch(train_data, train_labels, 128); 
-print(train_labels.shape, test_labels.shape);
+      idx = np.concatenate((self.pos_idx[:num_pos], self.neg_idx[:num_neg]),
+                            axis=0); 
+      return self.train_data.iloc[idx], self.train_labels_onehot[idx]; 
+  
+
+VData = VolcanoData();
+
+data, labels = VData.get_next_batch(128); 
+print(data.shape);
+print(labels.shape);
--- volcanoes.py
+++ volcanoes.py
@@ -5,7 +5,7 @@ import os, glob
 import matplotlib
 matplotlib.use('Agg');
 from matplotlib import pyplot as plt
-from data import read_data, get_next_batch 
+from data import VolcanoData 
 from model import conv_net, get_regularization_error, get_weights
 import gc
 import imp
@@ -24,6 +24,7 @@ weights, biases, regularizer = get_weights();
 logits = conv_net(X, weights, biases, 1, True)
 prediction = tf.nn.softmax(logits)
 
+#entropy = tf.nn.weighted_cross_entropy_with_logits(logits=prediction, targets=Y, pos_weight=1)
 entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y)
 loss_op = tf.reduce_sum(entropy)
 
@@ -31,7 +32,7 @@ loss_op = tf.reduce_sum(entropy)
 diff_sq = tf.multiply((logits - Y),(logits - Y))
 
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-#optimizer = tf.train.RMSPropOptimizer(learning_rate=1e-2);
+#optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate);
 train_op = optimizer.minimize(loss_op + beta*get_regularization_error());
 
 correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
@@ -54,27 +55,26 @@ def run_epoch(sess, num_steps, initial_rate, anneal=False):
     training_loss = [];
     test_accuracy = [];
     test_tss = [];
-
-
-    train_data, train_labels, test_data, test_labels = read_data();
-    print(train_data.shape, train_labels.shape);
     
+    VObj = VolcanoData();
+
     for step in range(1, num_steps+1): 
         if anneal:
             rate = initial_rate * (1 - step/num_steps);
         
-        batch_x, batch_y = get_next_batch(train_data, train_labels, 128);
+        batch_x, batch_y = VObj.get_next_batch(batch_size=128);
             
         l,t= sess.run([loss_op, train_op], feed_dict={X: batch_x, 
                                                       Y: batch_y, 
                                                       learning_rate: rate})
         training_loss.append(l);
         
-        tss_score, acc = sess.run([tss, accuracy], feed_dict = {X: test_data,
-                                                Y: test_labels});
+        tps, fps, tns, fns, tss_score, acc = sess.run([tp, fp, tn, fn, tss, accuracy], feed_dict = {X: VObj.test_data,
+                                                Y: VObj.test_labels_onehot});
         test_accuracy.append(acc);
         test_tss.append(tss_score);
         print(l, acc, tss_score);
+        print(tps / (tps + fns), fns / (tps + fns), fps / (fps + tns), tns / (fps + tns));
               
     training_loss = np.array(training_loss); 
     
